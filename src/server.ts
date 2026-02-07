3e2fcfa73ed4ccd6f6fbb63d4f1c9b9c3c0d5b5a
distinkt-dd <dpemvr@yandex.ru>
2026-02-08 00:32:30 +0500

    Adding cors

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,12 @@ import portfolioItemRouter from './models/portfolio-item/portfolio-item.routes'
 dotenv.config()
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  credentials: false 
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
